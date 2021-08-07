9c4ce9c2deab1dcd909c7f3d425aa8719dae0757
Mélina <purple.melinaa@gmail.com>
2021-08-07 21:42:58 +0200

    port

--- index.js
+++ index.js
@@ -8,43 +8,38 @@ const app = express();
 app.use(formidable());
 app.use(cors());
 
-
 app.get("/", (req, res) => {
   res.json({ message: "Welcome on Marvel API !" });
 });
 
 app.get("/characters", async (req, res) => {
-try {
+  try {
     const response = await axios.get(
-        `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.MARVEL_API_KEY}&skip=${req.query.skip}&name=${req.query.name}`
-      );
-      // console.log(result.data);
-      res.json(response.data);
-} catch (error) {
+      `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.MARVEL_API_KEY}&skip=${req.query.skip}&name=${req.query.name}`
+    );
+    // console.log(result.data);
+    res.json(response.data);
+  } catch (error) {
     console.error(error);
-}
-    
+  }
 });
-
 
 app.get("/comics", async (req, res) => {
   try {
-      const response = await axios.get(
-          `https://lereacteur-marvel-api.herokuapp.com/comics/${id}?apiKey=${process.env.MARVEL_API_KEY}&skip=${req.query.skip}`
-        );
-        // console.log(result.data);
-        res.json(response.data);
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/comics/${id}?apiKey=${process.env.MARVEL_API_KEY}&skip=${req.query.skip}`
+    );
+    // console.log(result.data);
+    res.json(response.data);
   } catch (error) {
-      console.error(error);
+    console.error(error);
   }
-      
-  });
-
+});
 
 app.all("*", (req, res) => {
   res.json({ message: "Cette route n'existe pas !" });
 });
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3200, () => {
   console.log("Server Started");
 });
